dc139ae8a64c385b150ac3b833855236e3ed47e2
ThapeloMas <thapelo0744@gmail.com>
2024-07-30 15:06:49 +0200

    first commit

---
+++ src/components/search/Search.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../Api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+      const data = await response.json();
+      return {
+        options: data.data.map((city) => ({
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        })),
+      };
+    } catch (error) {
+      console.error(error);
+      return { options: [] };
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
