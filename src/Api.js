61493e4102b1735d04d5e162687c519d5d9fffcb
ThapeloMas <thapelo0744@gmail.com>
2024-07-31 11:30:15 +0200

    third commit

--- src/Api.js
+++ src/Api.js
@@ -1,14 +1,11 @@
 export const geoApiOptions = {
-   method: "GET",
-   headers: {
-     "x-rapidapi-key": "996645aadcmsh8c0253d90c3b7b0p166f6cjsne005cc058e18",
-     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-   },
- };
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "996645aadcmsh8c0253d90c3b7b0p166f6cjsne005cc058e18 ",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "e2d723e46d33ca07890d09c7058a7009"; 
-
-
-
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "./components/search/Search";
 import CurrentWeather from "./components/current-weather/Current-weather";
 import Forecast from "./components/forecast/Forecast";
@@ -8,6 +8,31 @@ import "./App.css";
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [searchedCities, setSearchedCities] = useState([]);
+  const [locationWeather, setLocationWeather] = useState(null);
+
+  useEffect(() => {
+    getCurrentLocationWeather();
+  }, []);
+
+  const getCurrentLocationWeather = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        fetch(
+          `${WEATHER_API_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setLocationWeather(data);
+          })
+          .catch(console.log);
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -22,19 +47,42 @@ function App() {
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
+        const forecastResponse = await response[1].json();
+
+        const cityWeather = {
+          city: searchData.label,
+          currentWeather: weatherResponse,
+          forecast: forecastResponse,
+        };
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+        setSearchedCities((prevCities) => [...prevCities, cityWeather]);
       })
       .catch(console.log);
   };
 
   return (
-    <div className="container">
+    <div className="containers">
+      <div className="current-location-weathers">
+        {locationWeather && (
+          <div>
+            <h2 >Current Location</h2>
+            <CurrentWeather data={locationWeather} />
+          </div>
+        )}
+      </div>
+      <br></br>
       <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
+      <br></br>
+      <button className="btn" onClick={() => setSearchedCities([])}>Clear Search</button>
+      {searchedCities.map((city, index) => (
+        <div key={index}>
+          <h2>{city.city}</h2>
+          <CurrentWeather data={city.currentWeather} />
+          <Forecast data={city.forecast} />
+        </div>
+      ))}
     </div>
   );
 }
